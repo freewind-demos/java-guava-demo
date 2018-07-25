a7576ac4631276b319b4e4fe3909edc279a0f6e0
freewind <nowindlee@gmail.com>
2018-07-25 23:26:30 +0800

    strings

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,9 +1,12 @@
 package demo;
 
+import com.google.common.base.Strings;
+
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(Strings.commonPrefix("abc", "abd"));
+        System.out.println(Strings.commonSuffix("123", "223"));
     }
 
 }
